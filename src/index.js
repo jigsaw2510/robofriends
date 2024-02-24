ec878d63501995b92e08380de5453dc55f995037
Catalin Gavrila <cata_bb92@yahoo.com>
2024-02-24 20:22:39 +0200

    implemented search logic

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './Card.js';
+import App from './App'
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { robots } from './robots'
 
 // Set the title before rendering React components
 document.title = 'Robofriends';
@@ -12,11 +11,7 @@ document.title = 'Robofriends';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    </div>
+      <App />
   </React.StrictMode>
 );
 
